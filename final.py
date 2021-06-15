3dd0cfa0d65820dca22e3657c3004c4047c2e2ed
Junyoung Ahn <zxzimin@naver.com>
2021-06-15 21:00:18 +0900

    final project

---
+++ final.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+import urllib.request
+from konlpy.tag import Okt
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import koco
+
+data = koco.load_dataset('korean-hate-speech', mode='train_dev')
+
+train_data = data["train"][0:3000]
+test_data = data["train"][3000:6000]
+
+train_data = pd.DataFrame(train_data,
+                    columns=['comments', 'contain_gender_bias', 'bias', 'hate', 'news_title'])
+
+test_data = pd.DataFrame(test_data,
+                    columns=['comments', 'contain_gender_bias', 'bias', 'hate', 'news_title'])
+
+train_data.replace('none', 0, inplace=True)
+train_data.replace('hate', 1, inplace=True)
+train_data.replace('offensive', 1, inplace=True)
+
+test_data.replace('none', 0, inplace=True)
+test_data.replace('hate', 1, inplace=True)
+test_data.replace('offensive', 1, inplace=True)
+
+train_data['comments'] = train_data['comments'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+train_data['comments'] = train_data['comments'].str.replace('^ +', "") # white space 데이터를 empty value로 변경
+train_data['comments'].replace('', np.nan, inplace=True)
+
+test_data['comments'] = test_data['comments'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","") # 정규 표현식 수행
+test_data['comments'] = test_data['comments'].str.replace('^ +', "") # 공백은 empty 값으로 변경
+
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+
+okt = Okt()
+
+X_train = []
+for sentence in train_data['comments']:
+    temp_X = okt.morphs(sentence, stem=True) # 토큰화
+    temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
+    X_train.append(temp_X)
+
+X_test = []
+for sentence in test_data['comments']:
+    temp_X = okt.morphs(sentence, stem=True) # 토큰화
+    temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
+    X_test.append(temp_X)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 3
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+# 전체 단어 개수 중 빈도수 2이하인 단어는 제거.
+# 0번 패딩 토큰을 고려하여 + 1
+vocab_size = total_cnt - rare_cnt + 1
+
+tokenizer = Tokenizer(vocab_size) 
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+y_train = np.array(train_data['hate'])
+y_test = np.array(test_data['hate'])
+
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_train, drop_train, axis=0)
+
+def below_threshold_len(max_len, nested_list):
+  cnt = 0
+  for s in nested_list:
+    if(len(s) <= max_len):
+        cnt = cnt + 1
+
+max_len = 30
+below_threshold_len(max_len, X_train)
+
+X_train = pad_sequences(X_train, maxlen = max_len)
+X_test = pad_sequences(X_test, maxlen = max_len)
+
+from tensorflow.keras.layers import Embedding, Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+model = Sequential()
+model.add(Embedding(vocab_size, 100))
+model.add(LSTM(128))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=60, validation_split=0.2)
+
+loaded_model = load_model('best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+def sentiment_predict(new_sentence):
+  new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+  score = float(loaded_model.predict(pad_new)) # 예측
+  if(score > 0.5):
+    print("{:.2f}% 확률로 긍정 댓글입니다.\n".format(score * 100))
+  else:
+    print("{:.2f}% 확률로 부정 댓글입니다.\n".format((1 - score) * 100))
